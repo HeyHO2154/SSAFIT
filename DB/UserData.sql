fc5324d08f755766b1204c1218d297e2c9f5ec36
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2024-11-11 10:21:37 +0900

    temp

--- DB/UserData.sql
+++ DB/UserData.sql
@@ -4,7 +4,22 @@ CREATE DATABASE ssafit;
 
 USE ssafit;
 
+#users < videos < videos_comment
+#예를들어, 유저에 대해 열려있는 비디오를 조인하고, 그 비디오에 해당하는 댓글들을 조인한다
+
 CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    point INT DEFAULT 0
+);
+
+CREATE TABLE videos (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    point INT DEFAULT 0
+);
+
+CREATE TABLE videos_comment (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id VARCHAR(255) NOT NULL,
     point INT DEFAULT 0
