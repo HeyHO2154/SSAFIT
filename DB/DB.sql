f920734a85931adefdf36ce9150c0a2ef175d8f9
허성준 <103251705+HeyHO2154@users.noreply.github.com>
2024-11-20 14:48:04 +0900

    ㄴㅁㅇㅇㄴㄴ

--- DB/DB.sql
+++ DB/DB.sql
@@ -19,29 +19,6 @@ CREATE TABLE videos (
 UPDATE videos SET views = videos.views+1 WHERE videoId = "브베뮤직비디오 2 - 매일 같은 하루";
 
 INSERT INTO videos (videoId, category, url) VALUES 
-<<<<<<< Updated upstream
-('브베뮤직비디오 1 - 할수있어 넌 너니까 (선택 자막)', '브베', 'https://www.youtube.com/watch?v=2hMNx5PCpGg&list=PLPY5X004VxTr33YKUKV5cKlqqxzunyO7i'),
-('브베뮤직비디오 2 - 매일 같은 하루', '브베', 'https://www.youtube.com/watch?v=xY4v7ZI2jsM'),
-('브베뮤직비디오 3 - 힘찬 스텝, 하나! 둘! 셋! 넷!', '브베', 'https://www.youtube.com/watch?v=GDc_W-urbiQ'),
-('브베뮤직비디오 4 - 발명가의 하루', '브베', 'https://www.youtube.com/watch?v=ehuOJmJMP8g'),
-('브베뮤직비디오 5 - 마음을 비우면 행복해져요', '브베', 'https://www.youtube.com/watch?v=nnfhy29sG7M'),
-('sexy 푸드섹서 공혁준의 피자 & 스파게티 & 스파게티 먹방','먹방','https://www.youtube.com/watch?v=45rCI5jEJKY&t=447s'),
-('sexy 푸드섹서 공혁준의 스시 & 우동 먹방','먹방','https://www.youtube.com/watch?v=nqzmobqNQB8'),
-('sexy 푸드섹서 공혁준의 해물찜 먹방','먹방','https://www.youtube.com/watch?v=u61TlWYWmaQ'),
-('(먹방) 돌아온 푸드섹서 공혁준','먹방','https://www.youtube.com/watch?v=frLO9hWAPgw'),
-('(먹방) 차돌박이 짬뽕 X 짜장 게살 볶음밥 마시기!','먹방','https://www.youtube.com/watch?v=lxMtXEaGK10'),
-('QWER - 고민중독 l Guitar Cover','아이돌','https://www.youtube.com/watch?v=V6gUaMZGyUo'),
-('QWER 내 이름 맑음 Official MV','아이돌','https://www.youtube.com/watch?v=AlirzLFEHUI'),
-('ROSÉ & Bruno Mars - APT. (Official Music Video)','아이돌','https://www.youtube.com/watch?v=ekr2nIex040'),
-('「더 월드」 풀버전 - 히키킹 작곡캠프 【엘시 쇼케이스&미니콘서트 ver.】','아이돌','https://www.youtube.com/watch?v=1w-RPKXVx2Y'),
-('융터르 - 우왁굳 반응','아이돌','https://www.youtube.com/watch?v=PBDsj8V83vc&t=56s'),
-('The Amazing Digital Circus - Your New Home (Extended)', '음악', 'https://www.youtube.com/watch?v=Hk-p6xcDUVE'),
-('[Playlist] 쇠질하다 덤벨 던져버려도 난 몰라','음악','https://www.youtube.com/watch?v=ZjXp9b1ZbqA&t=38s'),
-('I Want to Break Free (Queen) Performed In North Korea','음악','https://www.youtube.com/watch?v=jYvcdhCbzJw'),
-('THERE IS NOTHING WE CAN DO - NAPOLEON MEME (1 HOUR) ‪@provsoo‬','음악','https://www.youtube.com/watch?v=WvlCV4b9R1w'),
-('𝐏𝐥𝐚𝐲𝐥𝐢𝐬𝐭 | 심장이 쿵쾅거려도... 끝까지 강한 척한다. 더 퍼스트 슬램덩크 OST','음악','https://www.youtube.com/watch?v=I33sYkoxNhY');
-SELECT * FROM videos;
-=======
 ('주말에 하면 딱 좋은 팔 운동 루틴', '팔', 'https://www.youtube.com/watch?v=jhioxpxkces'),
 (':fire:출렁이는 팔뚝살:fire:빨리 빼려면 1달만 이 루틴하세요', '팔', 'https://www.youtube.com/watch?v=T-bVqdhqW2U'),
 (':sports_medal:요일별운동:sports_medal: 금요일 형님 조직으로 돌아오십쇼..', '팔', 'https://www.youtube.com/watch?v=URVOrAxBDq0'),
@@ -66,7 +43,6 @@ SELECT * FROM videos;
 (':sports_medal:요일별운동:sports_medal: 수요일 탄력있는 가슴, 겨드랑이 군살타파! 가슴 운동루틴', '가슴', 'https://www.youtube.com/watch?v=-V-Rdcn_ok8'),
 ('Lv.4 10분 만에 집에서 가슴 작살내는 루틴 (누구나 쉽게 가능)', '가슴', 'https://www.youtube.com/watch?v=c_5ENJWekbQ'),
 ('【가슴루틴】 가슴이 커지는 여자가슴 운동루틴! 헬스장 기구+덤벨 프로그램', '가슴', 'https://www.youtube.com/watch?v=TuqxAV6lzfg');
->>>>>>> Stashed changes
 
 CREATE TABLE comments (
     userId VARCHAR(255) NOT NULL,
